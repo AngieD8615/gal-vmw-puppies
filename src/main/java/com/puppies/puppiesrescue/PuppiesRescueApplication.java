bc56c8de1371bf9d43a4c02304fec7d500eb251c
AngieD8615 <adavidson1@gmail.com>
2021-05-17 09:33:34 -0600

    initial commit

---
+++ src/main/java/com/puppies/puppiesrescue/PuppiesRescueApplication.java
@@ -0,0 +1,13 @@
+package com.puppies.puppiesrescue;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PuppiesRescueApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PuppiesRescueApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/puppies/puppiesrescue/PuppiesRescueApplicationTests.java
@@ -0,0 +1,13 @@
+package com.puppies.puppiesrescue;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PuppiesRescueApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
